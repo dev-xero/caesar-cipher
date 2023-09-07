f6f018b94aa7fe4f82616a79c64f5a556c32d3dc
Xero kun <elo48858@gmail.com>
2023-09-07 17:30:05 +0100

    `FEAT` Setup base logging

--- log/log.go
+++ log/log.go
@@ -2,8 +2,16 @@ package log
 
 import (
 	"fmt"
+	"github.com/charmbracelet/lipgloss"
 )
 
 func PrintTitle(word string) {
-	fmt.Println(word)
+	var style = lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("231")).
+		Background(lipgloss.Color("#42B0F5")).
+		Width(22).
+		Align(lipgloss.Center)
+
+	fmt.Println(style.Render(word))
 }
