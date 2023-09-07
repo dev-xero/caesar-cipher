a639b3fefaca82cdae3a9cb8f2e8297045a8e176
Xero kun <elo48858@gmail.com>
2023-09-07 21:14:16 +0100

    `FEAT` Re-implemented cipher algorithm

--- utils/cipher.go
+++ utils/cipher.go
@@ -4,28 +4,39 @@ import (
 	"dev-xero/caesar-cipher/log"
 	"errors"
 	"os"
-	"strings"
+	"unicode"
 )
 
 func GenerateCipher(word string, shift int) string {
-	shiftedLetters := []rune{}
-	lowercaseWord := strings.ToLower(word)
+	encryptedText := ""
 
-	for i := 0; i < len(word); i++ {
-		char := lowercaseWord[i]
-
-		if char < 'a' || char > 'z' {
-			log.Error(errors.New("invalid letter encountered"))
+	for _, char := range word {
+		if unicode.IsLetter(char) {
+			shiftedChar := shiftChar(char, shift)
+			encryptedText += string(shiftedChar)
+		} else if unicode.IsDigit(char) {
+			shiftedDigit := shiftDigit(char, shift)
+			encryptedText += string(shiftedDigit)
+		} else {
+			log.Error(errors.New("invalid character encountered"))
 			os.Exit(1)
 		}
-
-		index := (int(char-'a') + shift) % 26
-		if index < 0 {
-			index += 26
-		}
-
-		shiftedLetters = append(shiftedLetters, rune('a'+index))
 	}
 
-	return string(shiftedLetters)
+	return encryptedText
+}
+
+func shiftChar(char rune, shift int) rune {
+	var base rune
+	if unicode.IsUpper(char) {
+		base = 'A'
+	} else {
+		base = 'a'
+	}
+
+	return ((char-base)+rune(shift)+26)%26 + base
+}
+
+func shiftDigit(char rune, shift int) rune {
+	return ((char-'0')+rune(shift)+10)%10 + '0'
 }
