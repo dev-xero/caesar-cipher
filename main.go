573bebbccb409f4e7b94c4a314eb288b77f6448e
Xero kun <elo48858@gmail.com>
2023-09-11 13:56:18 +0100

    `FEAT` Added export feature

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"dev-xero/caesar-cipher/log"
 	"dev-xero/caesar-cipher/utils"
 	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 )
+
+func exportResult(encryptedText string, shift int) {
+	subdirectory, filename := "exported", fmt.Sprintf("%s_%d.txt", encryptedText, shift)
+	fullPath := filepath.Join(subdirectory, filename)
+
+	if err := os.MkdirAll("exported", os.ModePerm); err != nil {
+		log.Error(errors.New("could not create directory"))
+		os.Exit(1)
+	}
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		log.Error(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	text := fmt.Sprintf("encrypted text: %s\ndecryption key: %d", encryptedText, -shift)
+	_, err = writer.WriteString(text)
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("File '%s' has been written successfully in subdirectory '%s'.\n", filename, subdirectory)
+}
 
 func main() {
 	log.Title("CAESAR CIPHER")
@@ -29,6 +63,10 @@ func main() {
 			fmt.Println("Text:", word)
 			fmt.Println("Shift:", shift)
 			log.Result(encryptedText)
+
+			if export {
+				exportResult(encryptedText, shift)
+			}
 		} else {
 			log.Error(errors.New("shift is not a valid integer"))
 			os.Exit(1)
